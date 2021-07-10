7843f1fde1832eac7e1a74ef7de4132beacb899f
Maksim Zolotoi <spacenear@ya.ru>
2021-07-10 23:44:59 +0300

    Update.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.scss';
+import cn from 'classnames';
 import data from './../../questions.json';
+
+const filters = Array.from(
+  data.slice(1).reduce((result, el) => {
+    el.tags?.forEach(tag => {
+      result.add(tag);
+    });
+
+    return result;
+  }, new Set()),
+);
 
 function App() {
   const [counter, setCounter] = useState(1);
   const [isAnswerOpen, setIsAnswerOpen] = useState(false);
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
   const [questions, setQuestions] = useState(shuffle(data.slice(1)));
+  const [activeFilters, setActiveFilters] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    const localFilters = window.localStorage.getItem('filters');
+
+    if (localFilters) {
+      try {
+        setActiveFilters(JSON.parse(localFilters));
+      } catch (e) {
+        setActiveFilters([]);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (activeFilters.length < 1) {
+      setQuestions(shuffle(data.slice(1)));
+    } else {
+      setQuestions(
+        shuffle(
+          data.slice(1).filter(el => {
+            for (let i = 0; i < activeFilters.length; i++) {
+              if (el.tags?.includes(activeFilters[i])) {
+                return true;
+              }
+            }
+
+            return false;
+          }),
+        ),
+      );
+    }
+
+    setCounter(1);
+    setActiveQuestionIndex(0);
+    setIsAnswerOpen(false);
+    window.localStorage.setItem('filters', JSON.stringify(activeFilters));
+  }, [activeFilters]);
 
   const goToNext = () => {
     setCounter(counter + 1);
     setIsAnswerOpen(false);
     setActiveQuestionIndex(activeQuestionIndex + 1);
+    setIsFiltersOpen(false);
   };
 
   const showAnswer = () => {
     setIsAnswerOpen(true);
+    setIsFiltersOpen(false);
   };
 
   const reset = () => {
@@ -23,6 +75,21 @@ function App() {
     setIsAnswerOpen(false);
     setActiveQuestionIndex(0);
     setQuestions(shuffle(questions));
+    setIsFiltersOpen(false);
+  };
+
+  const toggleFiltersModal = () => {
+    setIsFiltersOpen(!isFiltersOpen);
+  };
+
+  const toggleFilter = (el: string): void => {
+    if (activeFilters.includes(el)) {
+      activeFilters.splice(activeFilters.indexOf(el), 1);
+    } else {
+      activeFilters.push(el);
+    }
+
+    setActiveFilters(Array.from(activeFilters));
   };
 
   function shuffle(arr: any[]): any[] {
@@ -90,6 +157,12 @@ function App() {
         </div>
       </div>
       <div className={styles.buttons}>
+        <div
+          className={cn(styles.button, activeFilters.length > 0 && styles.filtersActive)}
+          onClick={toggleFiltersModal}
+        >
+          <i className="fas fa-filter"></i>
+        </div>
         {isAnswerOpen || (
           <div className={styles.button} onClick={showAnswer}>
             <i className="fas fa-question"></i>
@@ -105,6 +178,21 @@ function App() {
           </div>
         )}
       </div>
+      {isFiltersOpen && (
+        <div className={styles.filtersWrap}>
+          {filters.map((el: any) => {
+            return (
+              <div
+                className={cn(styles.filterButton, activeFilters.includes(el) && styles.active)}
+                onClick={() => toggleFilter(el)}
+                key={el}
+              >
+                {el}
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
